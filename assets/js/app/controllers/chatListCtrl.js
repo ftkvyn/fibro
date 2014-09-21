40cba72f2b516c4b6ee2f6b06a04aa5ef0e7b741
Andrei Marchenko <ftkvyn@gmail.com>
2014-09-21 13:46:18 +0200

    displaying chats list

--- assets/js/app/controllers/chatListCtrl.js
+++ assets/js/app/controllers/chatListCtrl.js
@@ -3,7 +3,16 @@ fibroApp.controller('ChatListController', ['$http', '$scope', function($http, $s
 	var me = $scope;
 
 	$scope.loadChats = function(){
-		$http.get('/api/message/chats')
+		// $http.get('/api/message/chats')
+		// .success(function(data){
+		// 	me.chats = data;
+		// })
+		// .error(function(data){
+		// 	console.log(data);
+		// 	alert('Error occured while loading chats list.');
+		// });
+
+		$http.get('/api/chats/0/10')
 		.success(function(data){
 			me.chats = data;
 		})
@@ -11,15 +20,15 @@ fibroApp.controller('ChatListController', ['$http', '$scope', function($http, $s
 			console.log(data);
 			alert('Error occured while loading chats list.');
 		});
+	}
 
-		$http.get('/api/chats/0/10')
-		.success(function(data){
-			console.log(data);
-		})
-		.error(function(data){
-			console.log(data);
-			alert('Error occured while loading chats list.');
-		});
+	$scope.getRoute = function(chat){
+		if(chat.targetUser){
+			return 'user/' + chat.targetUser.id;
+		}
+		if(chat.targetProject){
+			return 'project/' + chat.targetProject.id;
+		}
 	}
 	
 }]);
